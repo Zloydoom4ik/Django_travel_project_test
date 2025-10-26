5af4455dc38938e9d5c8e4d691f5b457b653fbaa
Zloydoom4ik <lenyapereponov1@gmail.com>
2025-10-27 01:41:24 +0400

    Add prieview for images

--- travel_project/places/admin.py
+++ travel_project/places/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
-
+    readonly_fields = ['preview']
+    
+    def preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;" />',
+                obj.image.url
+            )
+        return "Нет изображения"
+    
+    preview.short_description = 'Превью'
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
